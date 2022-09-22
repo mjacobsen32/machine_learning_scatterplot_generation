e3a7263ef09f751a431985e2f0755b0854eb5483
Matthew Jacobsen <matthewjacobsen1999@gmail.com>
2022-09-22 12:47:20 -0700

    random line creation

--- data_generation/create_scatterplots.py
+++ data_generation/create_scatterplots.py
@@ -1,3 +1,4 @@
+from tkinter.tix import Tree
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,7 +7,16 @@ import shutil
 import random
 
 PATH = '../data/scatterplots/one_degree/'
-
+RGB = ['blue', 'red', 'green']
+samples_LOW = 20
+samples_HIGH = 250
+one_degree_LOW = 1
+one_degree_HIGH = 60
+one_degree_b_LOW = 0
+one_degree_b_HIGH = 800
+STD_LOW = 1
+STD_HIGH = 100
+#(x,y), labels = scatter(func=blobs, n_samples=100, n_features=2, centers=1,cluster_std=.50,center_box=(-10,10),shuffle=True,random_state=None,return_centers=False)
 
 def main():
     try:
@@ -16,11 +26,29 @@ def main():
     os.mkdir(PATH)
 
     for i in range(0, 10):
-        #(x,y), labels = scatter(func=blobs, n_samples=500, n_features=20, centers=None,cluster_std=.50,center_box=(-10,10),shuffle=True,random_state=None,return_centers=False)
-        (x,y), labels = scatter(func=even_x_equation, eq=[2,0], n_samples=100, mean=0, std=3, minx=0, maxx=10)
-        plt.scatter(x,y)
-        plt.savefig(PATH+'line_'+str(i)+'.png')
-        plt.clf()
+        s = plt.figure(figsize=(6,6), clear=True)
+        plt.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
+        (x,y), labels = scatter(
+            func=even_x_equation, 
+            eq=[(-1*random.randrange(one_degree_LOW,one_degree_HIGH)/10),(random.randrange(one_degree_b_LOW,one_degree_b_HIGH)/10)], 
+            n_samples=random.randrange(samples_LOW,samples_HIGH), 
+            mean=0, 
+            std=random.randrange(STD_LOW,STD_HIGH), 
+            minx=0, 
+            maxx=100)
+        plt.scatter(x=x,y=y,c='Red')
+        (x,y), labels = scatter(
+            func=even_x_equation, 
+            eq=[(random.randrange(one_degree_LOW,one_degree_HIGH)/10),(random.randrange(one_degree_b_LOW,one_degree_b_HIGH)/10)], 
+            n_samples=random.randrange(samples_LOW,samples_HIGH), 
+            mean=0, 
+            std=random.randrange(STD_LOW,STD_HIGH), 
+            minx=0, 
+            maxx=100)
+        plt.scatter(x=x,y=y,c='Green')
+
+        s.savefig(PATH+'line_'+str(i)+'.png', )
+        s.clear()
 
 
 def scatter(func, **kwargs):
